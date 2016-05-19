a26d8a90e0cf38c4be79481a4f40c2bdeada2fda
Manuel Fidalgo <m.fidalgo.fierro@gmail.com>
2016-05-19 12:35:05 +0200

    Implemented the filesystem hello world

--- src/HelloWorld.c
+++ src/HelloWorld.c
@@ -1,1 +1,24 @@
-#include <linux/module.h>
+#include <linux/module.h>    /* Needed by all modules */
+#include <linux/kernel.h>    /* Needed for KERN_INFO */
+#include <linux/init.h>      /* Needed for the macros */
+#include <linux/pagemap.h>   /* PAGE_CACHE_SIZE */
+#include <linux/fs.h>        /* libfs stuff */
+#include <asm/atomic.h>      /* atomic_t stuff */
+#include <asm/uaccess.h>     /* copy_to_user */
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Angel Manuel Guerrero Higueras");
+
+static int __init init_hello(void)
+{
+    printk(KERN_INFO "Hello world\n");
+    return 0;
+}
+
+static void __exit cleanup_hello(void)
+{
+    printk(KERN_INFO "Goodbye world\n");
+}
+
+module_init(init_hello);
+module_exit(cleanup_hello);
