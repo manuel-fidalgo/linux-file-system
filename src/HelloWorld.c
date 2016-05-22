4b38f3baaf14636eebdc1e334cb08204d4d5ccd6
Manuel Fidalgo <m.fidalgo.fierro@gmail.com>
2016-05-22 22:14:23 +0200

    Implemented the default methods and super_oper/file_system structs

--- src/HelloWorld.c
+++ src/HelloWorld.c
@@ -9,15 +9,33 @@
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Manuel Fidalgo Fierro");
 
-static int __init init_hello(void)
-{
-    printk(KERN_INFO "Hello world\n");
-    return 0;
+static struct file_system_type assoofs_type = {
+	.owner = THIS_MODULE,
+	.name = "assoofs",
+	.mount = assoofs_get_super,
+	.kill_sb = kill_litter_super,
+	
+};
+static struct dentry * assoofs_get_super(struct file_system_type * fst, int flags, const char *devname, void *data){
+	return mount_bdeb(fts,flags,devname,data,assoofs_fill_super);
+}
+static int assoofs_fill_super(struct super_block *sb, void * data, int silent){
+	sb->s_blocksize = PAGE_CACHE_SIZE;
+	sb->s_blocksize_bits = PAGE_CACHE_SHIFT;
+	sb->s_magic = LFS_MAGIC;
+	sb->s_op = &assoofs_s_ops;
 }
 
-static void __exit cleanup_hello(void)
-{
-    printk(KERN_INFO "Goodbye world\n");
+
+static int __init assoofs_init(void){
+
+    printk(KERN_INFO "Init \n");
+    return register_filesystem(&assoofs_type);
+}
+
+static void __exit cleanup_hello(void){
+
+    printk(KERN_INFO "End\n");
 }
 
 module_init(init_hello);
