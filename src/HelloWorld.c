e53ee7c37963d521915b7ca396a76648bb796d43
Manuel Fidalgo <m.fidalgo.fierro@gmail.com>
2016-05-23 20:33:45 +0200

    Implemented new file method

--- src/HelloWorld.c
+++ src/HelloWorld.c
@@ -9,6 +9,8 @@
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Manuel Fidalgo Fierro");
 
+static atomic_t counter;
+
 static struct file_system_type assoofs_type = {
 	.owner = THIS_MODULE,
 	.name = "assoofs",
@@ -16,14 +18,45 @@ static struct file_system_type assoofs_type = {
 	.kill_sb = kill_litter_super,
 	
 };
+
+static struct super_operations assoofs_s_ops = {
+	.statfs = simple_statfs,
+	.drop_inode = generic_delete_inode,
+};
+
 static struct dentry * assoofs_get_super(struct file_system_type * fst, int flags, const char *devname, void *data){
 	return mount_bdeb(fts,flags,devname,data,assoofs_fill_super);
 }
+
 static int assoofs_fill_super(struct super_block *sb, void * data, int silent){
 	sb->s_blocksize = PAGE_CACHE_SIZE;
 	sb->s_blocksize_bits = PAGE_CACHE_SHIFT;
 	sb->s_magic = LFS_MAGIC;
 	sb->s_op = &assoofs_s_ops;
+}
+
+static void assoofs_create_files(struct super_block *sb, struct dentry * root){
+
+	/**/
+	atomic_set(&counter,0);
+	assoofs_create_file(sb,root,"counter",&counter);
+	/**/
+}
+static struct dentry  assoofs_create_file(struct super_block *sb, struct dentry *dir, const char * name, atomic_t * counter){
+	struct dentry * dentry;
+	struct inode * inode;
+	struct qstr qname;
+	qname.name = name;
+	qname.len = strlen(name);
+	qname.hash = full_name_hash(name, qname.len);
+	dentry = d_alloc(dir, &qname);
+	if(!dentry) //goto out;?
+	inode = assofs_make_inode(sb,S_IFREG | 0644);
+	if(!inode) goto out_dput;
+	inode->i_fop = &assofs_file_ops;
+	inode-> i_private = counter;
+	d_add(dentry,inode);
+	return dentry;
 }
 
 
