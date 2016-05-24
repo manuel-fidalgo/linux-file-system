88b3a9388ad22771293239f1c4f46d6594565e6e
Manuel Fidalgo <m.fidalgo.fierro@gmail.com>
2016-05-24 18:57:53 +0200

    Headers added

--- src/HelloWorld.c
+++ src/HelloWorld.c
@@ -19,8 +19,23 @@ MODULE_AUTHOR("Manuel Fidalgo Fierro");
 /**
  Hay que defnir un superbloque inicializarlo y configurarlo
 */
+ /*HEADERS*/
+ static struct dentry * assoofs_get_super(struct file_system_type * fst, int flags, const char *devname, void *data);
+ static int assoofs_fill_super(struct super_block *sb, void * data, int silent);
 
-/*Pertenceinte a la libreria fs.h*/
+ static void assoofs_create_files(struct super_block *sb, struct dentry * root);
+ static struct dentry  assoofs_create_file(struct super_block *sb, struct dentry *dir, const char * name, atomic_t * counter);
+ static struct dentry  assoofs_create_directory(struct super_block *sb, struct dentry *dir, const char * name, atomic_t * counter);
+
+ static struct inode * assoofsmakeinode(struct superblock * sb, int mode);
+
+ static int assoofs_open(struct inode * inode, struct file *flip);
+ static int assoofs_read_file(struct file * flip, char * buf, size_t count, loff_t * offset);
+ static ssize_t assoofs_write_file(struct file * flip, const char * buf, size_t count,loff_t * offset);
+/*/HEADERS*/
+
+/*STRUCTS*/
+ /*Define el typo de sistema que hemos creado, tipo nombre y funciones que se llaman al montar y desmontar*/
 static struct file_system_type assoofs_type = {
 
 	.owner = THIS_MODULE,
@@ -30,7 +45,7 @@ static struct file_system_type assoofs_type = {
 	
 };
 
-/*estructura de las tareas que soporta el sistema*/
+/*Estructura de las tareas que soporta el sistema*/
 static struct super_operations assoofs_s_ops = {
 
 	.statfs = simple_statfs,
@@ -49,12 +64,12 @@ static struct file_operations assoofs_file_ops = {
 
 
 
-/*funcion que se llama al crear el superbloque*/
+/*Funcion que se llama al crear el superbloque*/
 static struct dentry * assoofs_get_super(struct file_system_type * fst, int flags, const char *devname, void *data){
 	return mount_bdev(fts,flags,devname,data,assoofs_fill_super);
 }
 
-/*llena el superbloque que se le pasa como puntero super_block*/
+/*Llena el superbloque que se le pasa como puntero super_block*/
 static int assoofs_fill_super(struct super_block *sb, void * data, int silent){
 
 	sb->s_blocksize = PAGE_CACHE_SIZE;
@@ -64,7 +79,8 @@ static int assoofs_fill_super(struct super_block *sb, void * data, int silent){
 	/*crear el root haciendo que el campo root del superbloque*/
 	//lalamas a la funcion create files pasandole el puntero raiz y el superbloque
 }
-/*creara los ficheros y las carpetas que se crean en el programa*/
+
+/*Creara los ficheros y las carpetas que se crean en el programa*/
 static void assoofs_create_files(struct super_block *sb, struct dentry * root){
 	static atomic_t counter;
 	/**/
@@ -72,7 +88,8 @@ static void assoofs_create_files(struct super_block *sb, struct dentry * root){
 	assoofs_create_file(sb,root,"counter",&counter); // crear un contador para cada fichero por separado;
 	/**/
 }
-/*crea un solo fichero, toma el superblo y se usa una estructura dentry(Entrad al directorio o algo asi) del dorecori cdonce lo queramos crear*/
+
+/*Crea un solo fichero, toma el superblo y se usa una estructura dentry(Entrad al directorio o algo asi) del dorecori cdonce lo queramos crear*/
 static struct dentry  assoofs_create_file(struct super_block *sb, struct dentry *dir, const char * name, atomic_t * counter){
 	struct dentry * dentry;
 	struct inode * inode;
@@ -92,13 +109,13 @@ static struct dentry  assoofs_create_file(struct super_block *sb, struct dentry 
 	d_add(dentry,inode); //añadimos la estructura dentry y el dentry,ufs capa intermedia que abstrae(clase abstracta que luego referecnia a una clase real)
 	return dentry; //devolvemos el struct dentry;
 }
-/*para la creacion de directorios*/
+
+/*Para la creacion de directorios*/
 static struct dentry assoofs_create_directory(struct super_block *sb, struct dentry *dir, const char * name, atomic_t * counter){
-	
+
 }
 
-/*funcion ara crear inodo*/
-//declaramos el nuevo inode e iniciamlizamos los valores
+/*funcion ara crear inodo, declaramos el nuevo inode e iniciamlizamos los valores*/
 static struct inode * assoofsmakeinode(struct superblock * sb, int mode){
 	
 	struct inode * ret; 
@@ -113,14 +130,11 @@ static struct inode * assoofsmakeinode(struct superblock * sb, int mode){
 	return ret;
 }
 
-/*leemos el contador scandolo del flip*/
-
-
-
 static int assoofs_open(struct inode * inode, struct file *flip){
 	flip->private_data = inode->i_private;
 	return 0;
 }
+
 /*letura de un fichero*/
 static int assoofs_read_file(struct file * flip, char * buf, size_t count, loff_t * offset){
 	atomic_t v;
@@ -145,6 +159,7 @@ static int assoofs_read_file(struct file * flip, char * buf, size_t count, loff_
 	*offset += count;
 	return count;
 }
+
 /*escritura de un fichero*/
 static ssize_t assoofs_write_file(struct file * flip, const char * buf, size_t count,loff_t * offset){
 	atomic_t * counter;
@@ -157,7 +172,6 @@ static ssize_t assoofs_write_file(struct file * flip, const char * buf, size_t c
 	atomic_set(counter, simple_strol(tmp,NULL,10));
 	return count;
 }
-
 
 static int __init assoofs_init(void){
 	
